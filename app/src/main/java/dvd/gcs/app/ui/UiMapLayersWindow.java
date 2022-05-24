f647763cff3da847900734ac842866faf49c162c
Yong Rui <tanyongruifenway@gmail.com>
2022-05-24 15:33:54 +0800

    Added UI mini window for layers  - Added UI mini window FXML for layers  - Added UI mini window Java for layers Updated UI mini window FXML for drone feed Added show and hide icons to an image folder in resources Updated stylesheet with more templates

---
+++ app/src/main/java/dvd/gcs/app/ui/UiMapLayersWindow.java
@@ -0,0 +1,22 @@
+package dvd.gcs.app.ui;
+
+import javafx.scene.control.TitledPane;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Component;
+
+@Component
+@Lazy
+public class UiMapLayersWindow extends UiElement<TitledPane> {
+    private static final String FXML = "UiMapLayersWindow.fxml";
+
+    private String title;
+
+    @Autowired
+    public UiMapLayersWindow(TitledPane titledPane) {
+        super(FXML, titledPane);
+        this.title = "Layers";
+        TitledPane root = this.getRoot();
+        root.setText(this.title);
+    }
+}
