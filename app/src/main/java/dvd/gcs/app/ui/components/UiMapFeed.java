e3534b788b361db3dff337974d7ce3e34ad1d20b
Yong Rui <tanyongruifenway@gmail.com>
2022-05-25 15:08:14 +0800

    Changed map UI java to extend from abstracted class instead

--- app/src/main/java/dvd/gcs/app/ui/components/UiMapFeed.java
+++ app/src/main/java/dvd/gcs/app/ui/components/UiMapFeed.java
@@ -1,6 +1,6 @@
 package dvd.gcs.app.ui.components;
 
-import dvd.gcs.app.ui.api.UiElement;
+import dvd.gcs.app.ui.api.UiPane;
 import javafx.scene.layout.Pane;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
@@ -8,11 +8,11 @@ import org.springframework.stereotype.Component;
 
 @Component
 @Lazy
-public class UiMapFeed extends UiElement<Pane> {
-    private static final String FXML = "UiMapFeed.fxml";
+public class UiMapFeed extends UiPane {
+    private static final String FXML = "UiMapFeed.fxml"; // TODO: currently unused (?)
 
     @Autowired
     public UiMapFeed(Pane pane) {
-        super(FXML, pane);
+        super(pane);
     }
 }
