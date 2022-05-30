66150d2b3f76a431377f09677a94b5f3f8951082
Ryan Tan <ryantanlien99@gmail.com>
2022-05-30 11:19:49 +0800

    Add back button styling
    Add CSS to back button

--- app/src/main/java/dvd/gcs/app/ui/components/video/UiBackButton.java
+++ app/src/main/java/dvd/gcs/app/ui/components/video/UiBackButton.java
@@ -3,6 +3,7 @@ package dvd.gcs.app.ui.components.video;
 import dvd.gcs.app.ui.api.UiButton;
 import dvd.gcs.app.ui.events.SwitchPaneEvent;
 import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
@@ -12,16 +13,26 @@ import org.springframework.stereotype.Component;
 public class UiBackButton extends UiButton {
 
     private static final String FXML = "UiBackButton.fxml";
+    private static final String ICON = "/images/left-arrow-icon.png";
+
 
     @Autowired
     public UiBackButton(Button button) {
         super(FXML, button);
         setButtonBehavior();
+        setButtonIcon();
     }
 
     private void setButtonBehavior() {
         this.getRoot().setOnAction(actionEvent -> {
             this.getRoot().fireEvent(new SwitchPaneEvent());
         });
+    }
+
+    private void setButtonIcon() {
+        ImageView iconImage = new ImageView(UiBackButton.ICON);
+        iconImage.setPreserveRatio(true);
+        iconImage.setFitHeight(25);
+        this.getRoot().setGraphic(iconImage);
     }
 }
