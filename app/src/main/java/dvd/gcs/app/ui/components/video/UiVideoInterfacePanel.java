6a78513d368de20ff47f9e5facec322731ff337b
Ryan Tan <ryantanlien99@gmail.com>
2022-05-31 15:56:06 +0800

    Update UiVideoInterfacePanel background

--- app/src/main/java/dvd/gcs/app/ui/components/video/UiVideoInterfacePanel.java
+++ app/src/main/java/dvd/gcs/app/ui/components/video/UiVideoInterfacePanel.java
@@ -1,7 +1,12 @@
 package dvd.gcs.app.ui.components.video;
 
 import dvd.gcs.app.ui.api.UiPane;
+import javafx.geometry.Insets;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.CornerRadii;
+import javafx.scene.paint.Paint;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
@@ -10,8 +15,8 @@ import org.springframework.stereotype.Component;
 @Lazy
 public class UiVideoInterfacePanel extends UiPane {
 
-    UiVideoInterfaceTop uiVideoInterfaceTop;
-    UiVideoInterfaceRight uiVideoInterfaceRight;
+    private final UiVideoInterfaceTop uiVideoInterfaceTop;
+    private final UiVideoInterfaceRight uiVideoInterfaceRight;
 
     @Autowired
     public UiVideoInterfacePanel(
@@ -21,7 +26,21 @@ public class UiVideoInterfacePanel extends UiPane {
         super(borderPane);
         this.uiVideoInterfaceTop = uiVideoInterfaceTop;
         this.uiVideoInterfaceRight = uiVideoInterfaceRight;
+        setProperties();
         fillInnerParts();
+    }
+
+    private void setProperties() {
+        BorderPane borderPane = (BorderPane) this.getRoot();
+        borderPane.setPrefHeight(Double.MAX_VALUE);
+        borderPane.setPrefWidth(Double.MAX_VALUE);
+        borderPane.setMaxWidth(Double.MAX_VALUE);
+        borderPane.setMaxHeight(Double.MAX_VALUE);
+        borderPane.setBackground(
+                new Background(
+                        new BackgroundFill(Paint.valueOf("666666"),
+                        new CornerRadii(0),
+                        new Insets(0))));
     }
 
     private void fillInnerParts() {
