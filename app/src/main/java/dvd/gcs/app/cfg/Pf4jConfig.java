d629d382e1cd86b416fb7efa6aca5cce3f94649b
Ryan Tan <ryantanlien99@gmail.com>
2022-07-12 16:58:25 +0800

    Recomment Luciad classes

--- app/src/main/java/dvd/gcs/app/cfg/Pf4jConfig.java
+++ app/src/main/java/dvd/gcs/app/cfg/Pf4jConfig.java
@@ -83,7 +83,7 @@ public class Pf4jConfig {
 
 
         // Luciad Lightspeed extension
-/*        List<LuciadMapInterface> luciadMaps = pluginManager.getExtensions(LuciadMapInterface.class);
+        List<LuciadMapInterface> luciadMaps = pluginManager.getExtensions(LuciadMapInterface.class);
         System.out.println("Luciad size: " + luciadMaps.size());
         for (LuciadMapInterface luciadLightspeedMap: luciadMaps) {
             // Load SwingNode from plugin
@@ -102,7 +102,7 @@ public class Pf4jConfig {
         System.out.println("Greeting size: " + greetings.size());
         for (Greeting greeting: greetings) {
             System.out.println(greeting.getGreeting());
-        }*/
+        }
     }
 
     /**
